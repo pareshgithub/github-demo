34b56fd9d7bd346b4ab53365e8f873f54a8decdb
pareshgithub <pareshgadre2012@gmail.com>
2020-08-25 17:15:49 -0400

    test removed

--- enhanced-shoppingcart/src/app/app.module.ts
+++ enhanced-shoppingcart/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CartComponent,
-    TestComponent
+    CartComponent
   ],
   imports: [
     BrowserModule,
