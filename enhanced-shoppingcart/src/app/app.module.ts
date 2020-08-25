fa9c0b3b206d35b51703de9cab1fc75aaf7793f0
pareshgithub <pareshgadre2012@gmail.com>
2020-08-25 17:08:28 -0400

    test

--- enhanced-shoppingcart/src/app/app.module.ts
+++ enhanced-shoppingcart/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CartComponent
+    CartComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
